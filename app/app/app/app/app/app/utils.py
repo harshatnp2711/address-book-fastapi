30bf7f991e56a6191ca5658a0ab2a82c31174268
harshatnp2711 <harshatnp2711@gmail.com>
2026-02-09 11:32:30 +0530

    Update utils.py

--- app/app/app/app/app/app/utils.py
+++ app/app/app/app/app/app/utils.py
@@ -1,1 +1,14 @@
 
+import math
+
+def calculate_distance(lat1, lon1, lat2, lon2):
+    R = 6371
+    dlat = math.radians(lat2 - lat1)
+    dlon = math.radians(lon2 - lon1)
+
+    a = math.sin(dlat/2)**2 + \
+        math.cos(math.radians(lat1)) * \
+        math.cos(math.radians(lat2)) * \
+        math.sin(dlon/2)**2
+
+    return R * 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
